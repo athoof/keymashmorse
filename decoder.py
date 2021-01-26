414b70fa327b9d334a05c5eefa5427ed115094e0
Athoof Riyaz <athoof@gmail.com>
2021-01-26 15:01:24 +0500

    Converted to decode() function

--- decoder.py
+++ decoder.py
@@ -1,16 +1,12 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz ";
-morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", " "];
+def decode(encodedText):
+  # Replace every occurrence s with . and k with -
+  decodedText = encodedText.replace('s', '.').replace('k', '-');
 
-inp = input();
+  # If character is alphanumeric, i.e not a dit or dah, return '0'
+  # .split('0') to delimit the resulting list by 0s, preserving spaces
+  decodedText = "".join(['0' if x.isalnum() else x for x in decodedText]).split('0');
 
-# Replace every occurrence s with . and k with -
-stripped = inp.replace('s', '.').replace('k', '-');
+  # using morse[] as reference to find the corresponding index on alphabet[]
+  decodedText = "".join([alphabet[morse.index(x)] for x in decodedText]);
 
-# If character is alphanumeric, i.e not a dit or dah, return '0'
-# .split('0') to delimit the resulting list by 0s, preserving spaces
-stripped = "".join(['0' if x.isalnum() else x for x in stripped]).split('0');
-
-# using morse[] as reference to find the corresponding index on alphabet[]
-out = "".join([alphabet[morse.index(x)] for x in stripped]);
-
-print(out);
+  return decodedText

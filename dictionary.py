e8eb0a26c122bf3845bca90eb6896f63a81166ea
Athoof Riyaz <athoof@gmail.com>
2021-01-26 14:55:43 +0500

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz ";
+morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", " "];
+keymash = ["sk", "ksss", "ksks", "kss", "s", "ssks", "kks", "ssss", "ss", "skkk", "ksk", "skss", "kk", "ks", "kkk", "skks", "kksk", "sks", "sss", "k", "ssk", "sssk", "skk", "kssk", "kskk", "kkss", " "];
+keymashExtra = ['d', 'f', 'j', 'l', 'h', 'g', 'a'];

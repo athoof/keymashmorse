0ece4dcf0fa245c383b0ea6e7da0f1efc931c42f
Athoof Riyaz <athoof@gmail.com>
2021-01-26 14:59:07 +0500

    Converted to a encode() function

--- encoder.py
+++ encoder.py
@@ -1,20 +1,13 @@
 import random;
-alphabet = "abcdefghijklmnopqrstuvwxyz ";
-morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", " "];
-keymash = ["sk", "ksss", "ksks", "kss", "s", "ssks", "kks", "ssss", "ss", "skkk", "ksk", "skss", "kk", "ks", "kkk", "skks", "kksk", "sks", "sss", "k", "ssk", "sssk", "skk", "kssk", "kskk", "kkss", " "];
 
-keymashExtra = ['d', 'f', 'j', 'l', 'h', 'g', 'a'];
+def encode(inputText):
+  # Convert to keymash list
+  encodedText = [keymash[alphabet.index(x)] for x in inputText];
 
-inp = input();
+  # Convert to string delimited by 0s
+  encodedText = '0'.join(encodedText);
+  # Replace whitespaces with a random element from keymashExtra
+  # then return as a string
+  encodedText = ''.join([x.replace('0', random.choice(keymashExtra)) for x in encodedText])
+  return(encodedText);
 
-# Convert to keymash list
-encode = [keymash[alphabet.index(x)] for x in inp];
-
-# Convert to string delimited by 0s
-encode = '0'.join(encode);
-
-# Replace whitespaces with a random element from keymashExtra
-# then return as a string
-encode = ''.join([x.replace('0', random.choice(keymashExtra)) for x in encode])
-
-print(encode);

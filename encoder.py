6e96f899f30cf5714a5e3543f397b57b84d4910f
Athoof Riyaz <athoof@gmail.com>
2021-01-27 17:34:25 +0500

    Fixed indents

--- encoder.py
+++ encoder.py
@@ -8,13 +8,13 @@ with open("dictionary.json", "r") as read_file:
     keymashExtra = data['keymashExtra']
 
 def encode(inputText):
-  # Convert to keymash list
-  encodedText = [keymash[alphabet.index(x)] for x in inputText];
+    # Convert to keymash list
+    encodedText = [keymash[alphabet.index(x)] for x in inputText];
 
-  # Convert to string delimited by 0s
-  encodedText = '0'.join(encodedText);
-  # Replace whitespaces with a random element from keymashExtra
-  # then return as a string
-  encodedText = ''.join([x.replace('0', random.choice(keymashExtra)) for x in encodedText])
-  return encodedText;
+    # Convert to string delimited by 0s
+    encodedText = '0'.join(encodedText);
+    # Replace whitespaces with a random element from keymashExtra
+    # then return as a string
+    encodedText = ''.join([x.replace('0', random.choice(keymashExtra)) for x in encodedText])
+    return encodedText;
 

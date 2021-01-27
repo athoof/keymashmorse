ee92d60ec959bbd1fc1145e376de8e74e82554c2
Athoof Riyaz <athoof@gmail.com>
2021-01-27 17:33:48 +0500

    Fixed indents

--- decoder.py
+++ decoder.py
@@ -7,14 +7,14 @@ with open("dictionary.json", "r") as read_file:
     keymashExtra = data['keymashExtra']
     
 def decode(encodedText):
-  # Replace every occurrence s with . and k with -
-  decodedText = encodedText.replace('s', '.').replace('k', '-');
+    # Replace every occurrence s with . and k with -
+    decodedText = encodedText.replace('s', '.').replace('k', '-');
 
-  # If character is alphanumeric, i.e not a dit or dah, return '0'
-  # .split('0') to delimit the resulting list by 0s, preserving spaces
-  decodedText = "".join(['0' if x.isalnum() else x for x in decodedText]).split('0');
+    # If character is alphanumeric, i.e not a dit or dah, return '0'
+    # .split('0') to delimit the resulting list by 0s, preserving spaces
+    decodedText = "".join(['0' if x.isalnum() else x for x in decodedText]).split('0');
 
-  # using morse[] as reference to find the corresponding index on alphabet[]
-  decodedText = "".join([alphabet[morse.index(x)] for x in decodedText]);
+    # using morse[] as reference to find the corresponding index on alphabet[]
+    decodedText = "".join([alphabet[morse.index(x)] for x in decodedText]);
 
-  return decodedText
+    return decodedText
